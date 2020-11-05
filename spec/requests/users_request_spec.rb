c30b79de87f98d16a46340051e69f723dacd95f4
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-11-05 09:54:41 -0500

    Added request tests to user index endpoint

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -6,5 +6,12 @@ RSpec.describe 'Users', type: :request do
       get users_path
       expect(response).to have_http_status(:success)
     end
+
+    it 'Returns an array of users' do
+      get('/api/users/')
+      json = JSON.parse(response.body)
+      puts json
+      expect(json['data']['users']).to contain_exactly()
+    end
   end
 end
