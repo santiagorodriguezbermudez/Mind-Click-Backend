32a4a24bd7f6962ecb53c457b2277b7c3ee3a1d6
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-11-10 22:08:26 -0500

    Refactor favorite controller removing instance variables

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,12 @@
 class FavoritesController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    @therapists = @user.favorites.joins(:therapist).pluck(
+    user = User.find(params[:user_id])
+    therapists_db = user.favorites.joins(:therapist).pluck(
       :id, :therapist_id, 'therapists.full_name', 'therapists.fee', 'therapists.description',
       'therapists.years_experience',
       'therapists.therapy_description'
     )
-    @therapists = @therapists.map do |therapist|
+    @therapists = therapists_db.map do |therapist|
       {
         favorite_id: therapist[0],
         id: therapist[1],
@@ -21,25 +21,23 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    @favorite = Favorite.new(favorite_params)
+    favorite = Favorite.new(favorite_params)
 
-    if @favorite.save
-      @therapist = Therapist.find(@favorite.therapist_id)
-      @user = User.find(@favorite.user_id)
+    if favorite.save
       response = { message: 'Favorite added succesfully' }
       json_response(response)
     else
-      json_response({ message: @favorite.errors.messages }, 422)
+      json_response({ message: favorite.errors.messages }, 422)
     end
   end
 
   def destroy
-    @favorite = Favorite.find(params[:id])
-    if @favorite.destroy
+    favorite = Favorite.find(params[:id])
+    if favorite.destroy
       response = { message: 'Favorite removed succesfully' }
       json_response(response)
     else
-      json_response({ message: @favorite.errors.messages }, 422)
+      json_response({ message: favorite.errors.messages }, 422)
     end
   end
 
